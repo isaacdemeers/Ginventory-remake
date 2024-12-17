145c8a351c441f20514cf78c0b150a58dbbafa92
Isrem_ <32309148+isaacdemeers@users.noreply.github.com>
2024-12-17 23:19:04 +0100

    Leaves

--- src/components/callToAction.tsx
+++ src/components/callToAction.tsx
@@ -1,14 +1,22 @@
 
 import Image from "next/image";
-
-import Logo from "@img/Ginventory/logo-ginventory-simple.png";
-import { BS_Light, BS_Medium } from "@/lib/fonts";
-import GetApp from "./getApp";
 import Link from "next/link";
 
+import { BS_Light, BS_Medium } from "@/lib/fonts";
+
+import GetApp from "./getApp";
+
+import Logo from "@img/Ginventory/logo-ginventory-simple.png";
+import LogoApple from "@img/Ginventory/leaf.png";
+
 export default function CallToAction() {
+
+
     return (
-        <section className={`${BS_Medium.className} w-full flex items-center justify-center flex-col gap-12  py-10`}>
+        <section className={`${BS_Medium.className} relative w-full flex items-center justify-center flex-col gap-12  py-10`}>
+            <Image src={LogoApple} alt="Ginventory Logo" className="hidden absolute top-0 left-[-60%] -rotate-45 md:block" width={680} />
+            <Image src={LogoApple} alt="Ginventory Logo" className="hidden absolute top-0 right-[-60%]  rotate-[150deg] md:block" width={680} />
+
             <Image src={Logo} alt="Ginventory Logo" width={73} className="mb-10" />
             <div className="flex items-center justify-center flex-col gap-4">
                 <h1 className={`text-3xl font-bold text-text ${BS_Light.className}`}>Just test Ginventory, you won't regret it</h1>
