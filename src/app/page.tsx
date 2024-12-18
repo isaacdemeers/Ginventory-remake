d39db0155a9cd5d92af2b7eff7acc73904307157
Isrem_ <32309148+isaacdemeers@users.noreply.github.com>
2024-12-18 09:19:06 +0100

    Install Framer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Image from "next/image";
 import Header from "@/components/header";
 import GetApp from "@/components/getApp";
@@ -6,6 +8,7 @@ import Infos from "@/components/infos";
 import CallToAction from "@/components/callToAction";
 
 import { BS_Light, BS_Light_Italic } from "@/lib/fonts";
+import { motion } from "motion/react"
 
 export default function Home() {
   return (
@@ -15,6 +18,12 @@ export default function Home() {
 
       <section className="flex items-center justify-center flex-col w-full h-fit gap-10 py-10">
         <h1 className={`${BS_Light.className} text-9xl text-text text-center font-bold tracking-wider`}>Find your next favorite <span className={`${BS_Light_Italic.className} text-9xl text-text text-center font-bold tracking-wider`}>Gin</span></h1>
+
+        <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+        />
         <GetApp />
       </section>
 
